94c53788fc80d48c07167fd5c101be77badf0677
DIANABEVA <diananalule5@gmail.com>
2022-06-20 16:44:28 +0300

    second ommit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,5 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("<h1>home</h1>")
