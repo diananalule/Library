f654d3f99e5cbbfff62c4459ca0a3182f723d971
BasiimaCollinelius <collynzbasiima01@gmail.com>
2022-06-21 12:45:47 +0300

    update on views in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Login_view(request):
+    return
+
+
+def Logout_view(request):
+    return
+
+
+def Register_view(request):
+    return
