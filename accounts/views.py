10c88ab045976c5a6701fce8ba0a2ffba2ced1b0
BasiimaCollinelius <collynzbasiima01@gmail.com>
2022-06-22 12:23:20 +0300

    some changes made in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import views
 # Creating the views here.
 
 
 def Login_view(request):
-    return
+    return HttpResponse('Please login from here')
 
 
 def Logout_view(request):
-    return
+    return HttpResponse('please logout from here')
 
 
 def Register_view(request):
-    return
+    return HttpResponse('please register from here')
