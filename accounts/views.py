ed4e95da5ed08ab9e63bbe27625b5a7a5dd3a1fe
javious_katalemwa <katalemwa17@gmail.com>
2022-06-28 19:04:52 +0300

    user authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 #from . import views
 # Creating the views here.
 
 
 def Login_view(request):
-    return render(request,'accounts/index.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            context = {"error": "Inavalid username or password"}
+            return render(request, 'accounts/login.html', context)
+        login(request, user)
+        return redirect("/")
+        print(username, password)
+    return render(request, 'accounts/login.html', {})
 
 
 def Logout_view(request):
-    return render(request,'accounts/logout_view.html')
+    if request.method == "POST":
+        logout(request)
+    return redirect("/login")
+    return render(request, 'accounts/logout_view.html', {})
 
 
 def Register_view(request):
-    return render(request,'accounts/register_view.html')
+    return render(request, 'accounts/register_view.html', {})
--- project1/urls.py
+++ project1/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import Login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include("myapp.urls")),
-    path('accounts/', include("accounts.urls"))
+    path('accounts/', include("accounts.urls")),
+    path('login/', Login_view)
 ]
