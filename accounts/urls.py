8403fa739509d1c4173ed6aba3e76f91367755a5
DIANABEVA <diananalule5@gmail.com>
2022-06-30 03:50:54 +0300

    removed / symbol on login

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/login', views.Login_view, name='Login_view'),
+    path('login', views.Login_view, name='Login_view'),
     path('', views.Logout_view, name='Logout_view'),
     path('', views.Register_view, name='Register_view')
 ]
