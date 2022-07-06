654a8479d5e1cc9174a5e8b72ec4b9631e3636f9
demivivi <nakalanzivivian69@gmail.com>
2022-07-06 16:41:52 +0300

    modified models in myapp

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
     
-class Book(models.Model):
+class Books(models.Model):
     title=models.CharField(max_length=100)
     Author=models.CharField(max_length=100)
     updated=models.DateTimeField(auto_now_add=True) 
