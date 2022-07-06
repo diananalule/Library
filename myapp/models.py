0577e43fbb0b7a57d8bc9904cd6376961e07006b
demivivi <nakalanzivivian69@gmail.com>
2022-07-06 16:10:02 +0300

    Started on models

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,19 @@
-from datetime import datetime
 from django.db import models
 
-class Table(models.Model):
-    name = models.CharField(max_length=30)
+
+
+    
+class Book(models.Model):
+    title=models.CharField(max_length=100)
+    Author=models.CharField(max_length=100)
+    updated=models.DateTimeField(auto_now_add=True) 
+    Publication_date=models.DateField()
+    Subject_area=models.CharField(max_length=100)
+    
+    
+    
+    
+    
     
     
     
