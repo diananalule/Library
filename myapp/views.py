969d3a5a71bc1eab7c993c051889d8a899febeb4
BasiimaCollinelius <collynzbasiima01@gmail.com>
2022-07-06 17:59:38 +0300

    modified views in myapp

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="/login/")
 def home(request):
     return render(request, "accounts/index.html")
