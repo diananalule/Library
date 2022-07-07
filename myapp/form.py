7c6298b5a3fe6791bf934c4e1080121a670076b5
demivivi <nakalanzivivian69@gmail.com>
2022-07-07 13:33:20 +0300

    Created form for Borrower model

--- myapp/form.py
+++ myapp/form.py
@@ -1,3 +1,9 @@
 from django.forms import ModelForm
 from django import forms
 
+class BorrowerForm(forms.Form):
+    name = forms.CharField(max_length=200)
+    phone = forms.CharField(max_length=200)
+    email= forms.CharField(max_length=200,null=True)
+    return_date = forms.DateTimeField(auto_now_add=True)
+    due_date = forms.DateField()
