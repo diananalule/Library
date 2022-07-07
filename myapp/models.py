a696fea4108ddacc380b5fccc11c5553559761fc
DIANABEVA <diananalule5@gmail.com>
2022-07-07 13:11:17 +0300

    I have made some others modifications in models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,4 @@
-from re import T
-import re
-from sre_constants import CATEGORY
+
 from django.db import models
 class Book(models.Model):
     STATUS = [
@@ -23,11 +21,12 @@ class Book(models.Model):
 
 
 class Borrower(models.Model):
-    name = models.CharField(max_length=200,null=True)
+    name = models.CharField(max_length=200)
     phone = models.CharField(max_length=200,null=True)
     email= models.CharField(max_length=200,null=True)
     return_date = models.DateTimeField(auto_now_add=True)
     due_date = models.DateField()
+    cover = models.ImageField(upload_to="images",blank =True)
 
     def __str__(self):
         return self.name
