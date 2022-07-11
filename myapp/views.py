0bdeecdd369d2a16eb8fc3771c29b6a846f02d19
DIANABEVA <diananalule5@gmail.com>
2022-07-11 18:07:09 +0300

    added bootstrap templates

--- myapp/views.py
+++ myapp/views.py
@@ -7,12 +7,20 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="/login/")
 def home(request):
-    return render(request,"accounts/index.html")
+    books = Book.objects.all()  # gets all books
+    context = {'books': books}
+    return render(request, "accounts/index.html", context)
+
 
 def about_view(request):
-    return render(request,"accounts/about.html")
+    return render(request, "accounts/about.html")
+
 
 def service_view(request):
-    return render(request,"accounts/service.html")
+    return render(request, "accounts/service.html")
 
 
+def showBook(request, pk):
+    book = Book.objects.get(id=pk)
+    context = {'book': book}
+    return render(request, 'accounts/show_book.html', context)
