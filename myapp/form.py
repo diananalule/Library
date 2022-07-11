776998fb925f59974675310fe0f8d4e9256e7497
demivivi <nakalanzivivian69@gmail.com>
2022-07-11 14:41:17 +0300

    made form for book model

--- myapp/form.py
+++ myapp/form.py
@@ -1,3 +1,13 @@
 #from django.forms import ModelForm
+from random import choices
+from telnetlib import STATUS
 from django import forms
 from .models import Book
+
+class BookForm(forms.ModelForm):
+    title = forms.CharField(max_length=100)
+    author = forms.CharField(max_length=100)
+    updated = forms.DateTimeField(auto_now_add=True)
+    publication_date = forms.DateField()
+    status = forms.CharField(max_length=30, choices=STATUS)
+    subject_area = forms.CharField(max_length=100)
